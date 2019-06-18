44e049a4681b9dee4bed0843bbee68cd629ea855
ihoneysec <102505481@qq.com>
2019-06-18 19:27:31 +0800

    增加IpInfo

--- lib/utils/isp.py
+++ lib/utils/isp.py
@@ -5,23 +5,24 @@ import json
 
 
 def getIpInfo(arg):
-    ipInfoResult = arg[0]
+    ipInfoResult = ''
     try:
-        url = "http://ip.taobao.com/service/getIpInfo.php?ip=%s" % str(ipInfoResult)
+        url = "http://ip.taobao.com/service/getIpInfo.php?ip=%s" % str(arg[0])
         s = requests.get(url, headers=HEADERS, timeout=TIMEOUT, verify=VERIFY).text
         jsondata = json.loads(s)
 
         if jsondata['code'] == 1:
-            ipInfoResult += ' ' + 'Error'
+            arg.append('getIpInfoError')
         else:
             if jsondata['data']['region'] and jsondata['data']['region'] != 'XX':
-                ipInfoResult += ' ' +jsondata['data']['region']
+                ipInfoResult += jsondata['data']['region']
             if jsondata['data']['city'] and jsondata['data']['city'] != 'XX':
-                ipInfoResult += ' ' + jsondata['data']['city']
+                ipInfoResult += ' ' +jsondata['data']['city']
             if jsondata['data']['isp'] and jsondata['data']['isp'] != 'XX':
-                ipInfoResult += ' ' + jsondata['data']['isp']
-        if ipInfoResult:
-            return [ipInfoResult]
+                ipInfoResult += ' ' +jsondata['data']['isp']
+            arg.append(ipInfoResult)
+        if arg:
+            return arg
     except Exception as e:
         print(e)
 
